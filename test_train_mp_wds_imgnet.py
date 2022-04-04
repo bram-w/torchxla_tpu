44266dac59f2f2589c7ea09fa99090ad4aeae6eb
bram-w <98437098+bram-w@users.noreply.github.com>
2022-04-04 08:35:20 -0700

    Adam --> AdamW

--- test_train_mp_wds_imgnet.py
+++ test_train_mp_wds_imgnet.py
@@ -350,7 +350,7 @@ def train_imagenet():
     writer = None
     if xm.is_master_ordinal():
         writer = test_utils.get_summary_writer(FLAGS.logdir)
-    optimizer = optim.Adam(
+    optimizer = optim.AdamW(
         model.parameters(),
         lr=FLAGS.lr,
         weight_decay=0.2,
