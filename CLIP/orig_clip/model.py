51e817c61c2620343054353ac6aa70bf7430c529
bram-w <b.wallace@salesforce.com>
2022-04-22 14:16:21 -0700

    disable conversion in layernorm

--- CLIP/orig_clip/model.py
+++ CLIP/orig_clip/model.py
@@ -262,12 +262,11 @@ class ModifiedResNet(nn.Module):
 
 class LayerNorm(nn.LayerNorm):
     """Subclass torch's LayerNorm to handle fp16."""
-
-    def forward(self, x: torch.Tensor):
+    def gforward(self, x: torch.Tensor):
         orig_type = x.dtype
         ret = super().forward(x.type(torch.float32))
         return ret.type(orig_type)
-
+    
 
 class QuickGELU(nn.Module):
     def forward(self, x: torch.Tensor):
